66510daa1d274c88db93b583c8c9e90d4101dbfb
Ben Shi <ben.shi@growsuper.com>
2019-11-02 13:53:15 +1100

    corda bootcamp state and contracts

--- src/main/java/bootcamp/TokenContract.java
+++ src/main/java/bootcamp/TokenContract.java
@@ -1,15 +1,51 @@
 package bootcamp;
 
 import net.corda.core.contracts.CommandData;
+import net.corda.core.contracts.CommandWithParties;
+import net.corda.core.contracts.Contract;
+import net.corda.core.contracts.ContractState;
+import net.corda.core.transactions.LedgerTransaction;
+import org.jetbrains.annotations.NotNull;
+
+import java.security.PublicKey;
+import java.util.List;
+
+import static net.corda.core.contracts.ContractsDSL.requireSingleCommand;
 
 /* Our contract, governing how our state will evolve over time.
  * See src/main/java/examples/ArtContract.java for an example. */
-public class TokenContract {
+public class TokenContract implements Contract {
     public static String ID = "bootcamp.TokenContract";
-
-
 
     public interface Commands extends CommandData {
         class Issue implements Commands { }
+        class Transfer implements Commands { }
+    }
+
+    @Override
+    public void verify(@NotNull LedgerTransaction tx) throws IllegalArgumentException {
+        CommandWithParties<TokenContract.Commands> command = requireSingleCommand(tx.getCommands(), TokenContract.Commands.class);
+        if (command.getValue() instanceof Commands.Issue) {
+            // checking shape
+            if (tx.getInputStates().size() > 0)
+                throw new IllegalArgumentException("Token issuance should have zero inputs.");
+            if (tx.getOutputStates().size() != 1)
+                throw new IllegalArgumentException("Token issuance should have one output.");
+
+            TokenState inputState = tx.outputsOfType(TokenState.class).get(0);
+            TokenState outputState = tx.outputsOfType(TokenState.class).get(0);
+
+            if (outputState.getAmount() <= 0)
+                throw new IllegalArgumentException("Token issurance should have an amojnt of more than 0");
+
+            final List<PublicKey> requiredSigners = command.getSigners();
+            if (!requiredSigners.contains(inputState.getIssuer().getOwningKey()))
+                throw new IllegalArgumentException("Token issuance should have issuer as the required signer in the input");
+            if (!requiredSigners.contains(outputState.getIssuer().getOwningKey()))
+                throw new IllegalArgumentException("Token issuance should have issuer as the required signer in the output");
+
+        } else {
+            throw new IllegalArgumentException("Unexpected command for token contract");
+        }
     }
 }
--- src/main/java/bootcamp/TokenState.java
+++ src/main/java/bootcamp/TokenState.java
@@ -1,5 +1,6 @@
 package bootcamp;
 
+import com.google.common.collect.ImmutableList;
 import net.corda.core.contracts.BelongsToContract;
 import net.corda.core.contracts.ContractState;
 import net.corda.core.identity.AbstractParty;
@@ -11,7 +12,46 @@ import java.util.List;
 
 /* Our state, defining a shared fact on the ledger.
  * See src/main/java/examples/ArtState.java for an example. */
-//@BelongsToContract(TokenContract.class)
-public class TokenState {
+@BelongsToContract(TokenContract.class)
+public class TokenState implements ContractState {
 
+    private Party issuer;
+    private Party owner;
+    private int amount;
+
+    public TokenState(Party issuer, Party owner, int amount) {
+        this.issuer = issuer;
+        this.owner = owner;
+        this.amount = amount;
+    }
+
+    public Party getIssuer() {
+        return issuer;
+    }
+
+    public void setIssuer(Party issuer) {
+        this.issuer = issuer;
+    }
+
+    public Party getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Party owner) {
+        this.owner = owner;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    @NotNull
+    @Override
+    public List<AbstractParty> getParticipants() {
+        return ImmutableList.of(issuer, owner);
+    }
 }
